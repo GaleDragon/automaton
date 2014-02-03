194baf03fc439bda5bb5d18663362de3362c6cdb
Jeremy Morgan <galedragon1223@gmail.com>
2014-02-03 15:38:54 -0500

    Changed the runner form to a simpler form

--- automaton/runner/views.py
+++ automaton/runner/views.py
@@ -1,32 +1,19 @@
-import os
-from django.conf import settings
 from django.shortcuts import render
-from runner.forms import TestCaseForm, CustomUploadHandler
+from runner.forms import TestSuiteInitForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 # Create your views here.
 @login_required
-@csrf_exempt
 def runner_view(request):
-    #request.upload_handlers.insert(0, CustomUploadHandler(upload_to=os.path.join(settings.MEDIA_ROOT, str(request.user), '%Y/%m/%d')) )
-    return _delegated_runner_view(request)
-
-@csrf_protect
-def _delegated_runner_view(request):
     if request.method == "POST":
-        form = TestCaseForm(request.POST, request.FILES)
+        form = TestSuiteInitForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
-            instance = form.instance
-            instance.runner = request.user
-            form.instance = instance
-            form.save()
+
             return HttpResponseRedirect( reverse("home") )
         else:
             return render(request, "runner.html", {"runner_form": form})
     else:
-        form = TestCaseForm()
+        form = TestSuiteInitForm()
         return render(request, "runner.html", {"runner_form": form})
