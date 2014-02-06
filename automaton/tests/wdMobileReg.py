32f7f9053c9ec038b180dac62ce8095060223d5e
Micah Jenkins <student0208@gmail.com>
2014-02-06 14:45:15 -0500

    added import names to modules that were using them

--- automaton/tests/wdMobileReg.py
+++ automaton/tests/wdMobileReg.py
@@ -5,6 +5,7 @@ import unittest
 from config import ConfigurationMixin
 import string
 import random
+import names
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
